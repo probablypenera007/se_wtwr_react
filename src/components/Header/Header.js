f38a7678bcedea129401d32ee99703ac2c025b63
James Penera <jamespenera@icloud.com>
2023-10-04 09:29:41 -0700

    set up header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import './Header.css';
+
+const Header = ({onCreateModal}) => {
+    const currentDate = new Date().toLocaleString('default',
+     { month: 'long', day: 'numeric' });
+    console.log('Header Here!');
+
+    return(
+        <header className='header'>
+        <div className='header__logo'>
+<div>
+    <img src={require("/Users/firefighterpenera/Desktop/TripleTen Files/se_wtwr_react/src/images/wtwrlogo.svg").default} alt='logo' />
+    </div>
+    <div>{currentDate}, Seattle</div>
+        </div>
+        <div className='header__avatar-logo'>
+        <div>
+          <button className='header__button-addClothes' type='text'> + Add Clothes</button>
+        </div>
+        <div className='header__name'>Terrence Tegegne</div>
+        <div>
+          <img src={require("/Users/firefighterpenera/Desktop/TripleTen Files/se_wtwr_react/src/images/avatar.svg").default} alt="avatar"/>
+        </div>
+        </div>   
+      </header> 
+    )
+}//{require("/Users/firefighterpenera/Desktop/TripleTen Files/se_wtwr_react/src/images/wtwrlogo.svg").default}
+
+export default Header;
+    
