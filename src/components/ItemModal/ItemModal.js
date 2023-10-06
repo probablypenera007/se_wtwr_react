a2a24c9bc680421547268fbf2c8c5796beed26a1
James Penera <jamespenera@icloud.com>
2023-10-05 23:19:46 -0700

    preview modal styled

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -1,16 +1,20 @@
+import "./ItemModal.css"
+
 const ItemModal = ({selectedCard, onClose}) => {
     console.log("i'm item modal")
 
     return (
         <div className={`modal`}>
-            <div className="modal__content">
-            <button className="button__close-modal" type='button'onClick={onClose}></button>
-              <img src={selectedCard.link} alt="selected-card"/> 
-              <div>{selectedCard.name}</div>
-              <div>{selectedCard.weather}</div>
+            <div className="modal__content-preview">
+            <button className="button__close-modal-white" type='button'onClick={onClose}></button>
+              <img src={selectedCard.link}
+              className="modal__image-preview"
+               alt="selected-card"/> 
+              <div className="modal__preview-name">{selectedCard.name}</div>
+              <div className="modal__preview-weather">Weather: {selectedCard.weather}</div>
       </div>
      </div>
     )
 }
 
-export default ItemModal;
+export default ItemModal;  
