1e243fea4591da740cb62133cc53df91726e387e
James Penera <jamespenera@icloud.com>
2023-10-07 01:48:34 -0700

    format utils

--- src/utils/WeatherOptions.js
+++ src/utils/WeatherOptions.js
@@ -1,14 +1,62 @@
 export const weatherOptions = [
-    { url: require("../images/day/cloudy-day.svg").default, day: true, type: "cloudyday"},
-    { url: require("../images/day/perfect-day.svg").default, day: true, type: "perfday"},
-    { url: require("../images/day/rainy-day.svg").default, day: true, type: "rainyday"},
-    { url: require("../images/day/stormy-day.svg").default, day: true, type: "stormyday"},
-    { url: require("../images/day/snowy-day.svg").default, day: true, type: "snowyday"},
-    { url: require("../images/day/foggy-day.svg").default, day: true, type: "foggyday"},
-    { url: require("../images/night/perfect-night.svg").default, day: false, type: "perfnight"},
-    { url: require("../images/night/cloudy-night.svg").default, day: false, type: "cloudynight"},
-    { url: require("../images/night/foggy-night.svg").default, day: false, type: "foggynight"},
-    { url: require("../images/night/rainy-night.svg").default, day: false, type: "rainynight"},
-    { url: require("../images/night/snowy-night.svg").default, day: false, type: "snowynight"},
-    { url: require("../images/night/stormy-night.svg").default, day: false, type: "stormynight"},
-]
+  {
+    url: require("../images/day/cloudy-day.svg").default,
+    day: true,
+    type: "cloudyday",
+  },
+  {
+    url: require("../images/day/perfect-day.svg").default,
+    day: true,
+    type: "perfday",
+  },
+  {
+    url: require("../images/day/rainy-day.svg").default,
+    day: true,
+    type: "rainyday",
+  },
+  {
+    url: require("../images/day/stormy-day.svg").default,
+    day: true,
+    type: "stormyday",
+  },
+  {
+    url: require("../images/day/snowy-day.svg").default,
+    day: true,
+    type: "snowyday",
+  },
+  {
+    url: require("../images/day/foggy-day.svg").default,
+    day: true,
+    type: "foggyday",
+  },
+  {
+    url: require("../images/night/perfect-night.svg").default,
+    day: false,
+    type: "perfnight",
+  },
+  {
+    url: require("../images/night/cloudy-night.svg").default,
+    day: false,
+    type: "cloudynight",
+  },
+  {
+    url: require("../images/night/foggy-night.svg").default,
+    day: false,
+    type: "foggynight",
+  },
+  {
+    url: require("../images/night/rainy-night.svg").default,
+    day: false,
+    type: "rainynight",
+  },
+  {
+    url: require("../images/night/snowy-night.svg").default,
+    day: false,
+    type: "snowynight",
+  },
+  {
+    url: require("../images/night/stormy-night.svg").default,
+    day: false,
+    type: "stormynight",
+  },
+];
--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -1,35 +1,36 @@
-
-
-const latitude = 47.60;
+const latitude = 47.6;
 const longitude = -122.33;
 const APIkey = `830c7046b70053a5bb6012f04397d976`;
 
-export const getForecastWeather =() => { 
-    const weatherApi = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`)
-    .then((res) => {if(res.ok){
-        if(res.ok) {
-            return res.json();
-        }else {
-            return Promise.reject(`Error: ${res.status}`)
-        }
-    }})
-    return weatherApi;
-}
+export const getForecastWeather = () => {
+  const weatherApi = fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${APIkey}`
+  ).then((res) => {
+    if (res.ok) {
+      if (res.ok) {
+        return res.json();
+      } else {
+        return Promise.reject(`Error: ${res.status}`);
+      }
+    }
+  });
+  return weatherApi;
+};
 
 export const parseWeatherData = (data) => {
-    console.log(data)
-    const main = data.main;
+  console.log(data);
+  const main = data.main;
 
-    const temperature= main && main.temp;
-    console.log("this temperature data", Math.ceil(temperature));
-    return Math.ceil(temperature);
-}
+  const temperature = main && main.temp;
+  console.log("this temperature data", Math.ceil(temperature));
+  return Math.ceil(temperature);
+};
 
 export const parseLocationData = (data) => {
-    const currentLocation = data.name;
-    console.log(currentLocation, "this is current location");
-    return currentLocation;
-}
+  const currentLocation = data.name;
+  console.log(currentLocation, "this is current location");
+  return currentLocation;
+};
 
 //export const weatherForecastData = (data) => {
 //    const currentForecast = data.weather;
@@ -48,7 +49,6 @@ export const parseLocationData = (data) => {
 //    }
 // ],
 
-
 //"dt": 1661870592,
 //"sys": {
 //    "type": 2,
@@ -58,13 +58,12 @@ export const parseLocationData = (data) => {
 //    "sunset": 1661882248
 //  },
 
-//For determining the time of day, you can compare Date.now() 
-//to the sunrise and sunset fields returned by the API. 
-//But be careful: Date.now() returns the time elapsed since January 1, 1970 in milliseconds. 
+//For determining the time of day, you can compare Date.now()
+//to the sunrise and sunset fields returned by the API.
+//But be careful: Date.now() returns the time elapsed since January 1, 1970 in milliseconds.
 //The weather API returns the time elapsed since January 1, 1970 in seconds.
 
 //export default weatherApi
-
 
 //  if (temperature >= 86) {
 //      return 'hot';
@@ -72,4 +71,4 @@ export const parseLocationData = (data) => {
 //      return 'warm';
 //    } else if (temperature <= 65) {
 //      return 'cold';
-//    }  
+//    }
