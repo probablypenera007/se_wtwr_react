2e0758ac6f4043d9c41ab73f999253d1faecd647
James Penera <jamespenera@icloud.com>
2023-10-10 23:41:18 -0700

    Create Api.js

---
+++ src/utils/Api.js
@@ -0,0 +1,16 @@
+// import React from "react";
+
+// const baseUrl = 'http://localhost:3001';
+
+// GET https://localhost:3001/items
+// Export the contents of api.js to the App component. 
+// Modify the corresponding Effect hook to add the items
+//  to the application state from the server
+
+
+// POST https://localhost:3001/items
+// Pass name, imageUrl, and weather to the request body. 
+// Modify the corresponding handler for adding a new item in App.js.
+
+
+// DELETE https://localhost:3001/items/:id
